20e8b18076a8451d260dd13071652336cb0ccff9
maicol <maicol.matteoli@gmail.com>
2021-05-31 14:01:03 +0200

    test array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // console.log("scriptttttttttttttttttttt");
+var lista = ["mail@lol.com", "viafarlocca@indirizzo.com", "numero22@gmail.com"];
+
+var mail = prompt("inserisci la mail : ");
+
+for(var i=0 ; i < lista.length ; i++){
+    console.log(lista[i]);
+}
